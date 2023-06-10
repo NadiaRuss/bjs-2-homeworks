9f57f6714e8c91421c8a5b1b9cc8c69b93a9e0b6
Надежда Яковлева <i160220@mail.ru>
2023-06-10 18:53:27 +0700

    внесены правки в кодстайл

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,24 +1,24 @@
 "use strict"
 function solveEquation(a, b, c) {
-  let arr = [];
-  let x;
-  let y;
-  let d = b**2 - 4*a*c;
-  if (d < 0) {
-    return arr; 
-  } else if (d === 0) {
-    x = -b/(2*a);
-    arr.push(x)
-  } else if (d > 0) {
-    x = (-b + Math.sqrt(d) )/(2*a);
-    y = (-b - Math.sqrt(d) )/(2*a);
-    arr.push(x, y);
-  }
-  return arr; 
+	let arr = [];
+	let x;
+	let y;
+	let dsrt = b ** 2 - 4 * a * c;
+	if (dsrt < 0) {
+		return arr;
+	} else if (dsrt === 0) {
+		x = -b / (2 * a);
+		arr.push(x)
+	} else {
+		x = (-b + Math.sqrt(dsrt)) / (2 * a);
+		y = (-b - Math.sqrt(dsrt)) / (2 * a);
+		arr.push(x, y);
+	}
+	return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  let P = percent/100/12;
+  let P = percent / 100 / 12;
   let n = countMonths;
   let S = amount - contribution;
   let monthlyFee = S * (P + (P / (((1 + P)**n) - 1)))
